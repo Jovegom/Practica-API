6b7da3b8f57d69826205131f94ba812fbe783694
JohanVega <johan.001997@gmail.com>
2025-10-15 21:53:14 +0000

    avance

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
+
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
 from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
 # Spotify API credentials
+
